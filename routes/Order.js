356814022d9bf3c7b365c37223b1b148e6ceb5e0
Ash_333 <akamat62@gmail.com>
2023-10-23 21:08:11 +0530

    added authorization in Order route

--- routes/Order.js
+++ routes/Order.js
@@ -3,7 +3,7 @@ const router=express.Router()
 const {auth,checkAdminStatus}=require('../middleware/auth')
 const {createNewOrder,getAllOrder,getOrderOfUser,updateOrderStatus}=require('../controller/Order')
 
-router.put('/order',auth,createNewOrder)
+router.post('/order',auth,createNewOrder)
 router.get('/order',auth,checkAdminStatus,getAllOrder)
 router.get('/order/:id',auth,getOrderOfUser)
 router.put('/order/:id/status',auth,checkAdminStatus,updateOrderStatus)
